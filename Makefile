1b103ac08bfdaeedd9e0c954038ad25c20c0e575
Eduard Vintila <eduard.vintila47@gmail.com>
2021-01-14 13:51:37 +0200

    modificat comentarii

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
 obj-m += userfs.o
 
 all:
-
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
-	
 clean:
-
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean 
--- userfs.c
+++ userfs.c
@@ -11,31 +11,37 @@
 
 #define USERFS_MAGIC 0x13371337
 #define USERFS_DIRS_OFFSET 1024
-#define MAX_USERS 512
-#define MAX_OUTPUT_SIZE 8192
+#define USERFS_MAX_USERS 512
+#define USERFS_MAX_OUTPUT_SIZE 8192
 #define COLOR_GREEN   "\x1b[32m"
 #define COLOR_RESET   "\x1b[0m"
 
-DEFINE_MUTEX(userfs_mutex_tura);
+DEFINE_MUTEX(userfs_mutex_turn);
 
-static long tura; /* TODO: de schimbat denumirea variabilei! */
-static long ture[MAX_USERS]; // poate ar merge mai bine un malloc la userfs_init?
+/* Ture: folosite pentru a nu repeta afisarea unui director de mai multe ori in aceeasi parcurgere */
+static long turn;
+static long turns[USERFS_MAX_USERS]; // poate ar merge mai bine un malloc la userfs_init?
 static int n;
 
-
+/* Copiaza in bufferul 'name' numele utilizatorului cu uid-ul dat.
+ * Daca nu exista, intoarce -1.
+ * Intoarce 0 la succes.
+ */ 
 static int get_username(kuid_t uid, char *name, int size)
 {
+	/* Formatul din /etc/passwd:   user:pass:uid:restul */
+	
 	struct file *f;
 	char buf[24]; // de verificat de ce nu merge cu 128!
+	char *it;
+	char username[32 + 1]; /* 32 e marimea maxima a numelui unui utilizator */
+	char str_uid[8];
 	loff_t pos = 0;
 	long l_uid;
 	kuid_t uid_passwd;
 	ssize_t bytes = 0;
-	char *it;
-	char username[32 + 1]; // 32 e marimea maxima a numelui unui utilizator
-	char str_uid[8];
-	int cnt = 0;
-	bool ok_nume = 0, ok_pass = 0, ok_uid = 0; // 0 - nu am terminat de citit, 1 - am citit
+	int cnt = 0; /* Contor cu ajutorul caruia retinem unde am ramas cu cititul */
+	bool ok_name = 0, ok_pass = 0, ok_uid = 0; /* 0 - nu am terminat de citit, 1 - citire finalizata */
 	
 	if (!name)
 		return -1;
@@ -43,7 +49,7 @@ static int get_username(kuid_t uid, char *name, int size)
 	f = filp_open("/etc/passwd", O_RDONLY, 0);
 	if (!f)
 	{
-		pr_err("Eroare deschidere /etc/passwd!\n");
+		pr_err("UserFS: Eroare deschidere /etc/passwd!\n");
 		goto err;
 	}
 	
@@ -52,56 +58,56 @@ static int get_username(kuid_t uid, char *name, int size)
 		buf[bytes] = '\0';
 		it = buf;
 		
-		if (ok_uid) // daca am terminat de citit uid-ul
+		if (ok_uid) /* Daca am terminat de citit uid-ul */
 		{
-			it = strchr(it, '\n'); // trecem la urmatoarea linie din fisier
+			it = strchr(it, '\n'); /* Trecem la urmatoarea linie din fisier */
 			if (!it)
-				continue; 
+				continue; /* Citim in continuare din fisier daca nu am gasit \n */
 			
-			it++; // trecem de '\n'
-			ok_nume = ok_pass = ok_uid = 0; // resetem ok-urile
-			cnt = 0; // si contorul
+			it++; /* Trecem de '\n' */
+			ok_name = ok_pass = ok_uid = 0; /* Resetem ok-urile */
+			cnt = 0; /* Si contorul */
 		} 
 		
-		if (!ok_nume) // daca nu am terminat de citit numele
+		if (!ok_name) /* Daca nu am terminat de citit numele */
 		{
 			for (; *it != ':' && *it != '\0'; cnt++, it++)
 				username[cnt] = *it;
 			
 			username[cnt] = '\0';
-			if (*it == '\0') // nu mai avem ce citi tura asta
+			if (*it == '\0') /* Nu mai avem ce citi din buffer */
 				continue;
 			
-			ok_nume = 1; // altfel, am dat de ':', deci am terminat de citit numele
+			ok_name = 1; /* Altfel, am dat de ':', deci am terminat de citit numele */
 			it++;
-			cnt = 0;
+			cnt = 0; /* Resetam contorul */
 		}
 		
-		if (!ok_pass)
+		if (!ok_pass) /* Daca nu am terminat de citit parola */
 		{
-			for (; *it != ':' && *it != '\0'; cnt++, it++); // sarim peste parola
+			for (; *it != ':' && *it != '\0'; cnt++, it++); /* Sarim peste parola */
 			
-			if (*it == '\0') // nu mai avem ce citi tura asta
+			if (*it == '\0') /* Nu mai avem ce citi din buffer */
 				continue;
 			
-			ok_pass = 1;
+			ok_pass = 1; /* Altfel, am dat de ':', deci am terminat de citit parola */
 			it++;
-			cnt = 0;
+			cnt = 0; /* Resetam contorul, desi nu l-am folosit pt parola */
 		}
 		
-		if (!ok_uid)
+		if (!ok_uid) /* Daca nu am terminat de citit uid-ul */
 		{
 			for (; *it != ':' && *it != '\0'; cnt++, it++)
 				str_uid[cnt] = *it;
 				
 			str_uid[cnt] = '\0';
-			if (*it == '\0') // nu mai avem ce citi tura asta
+			if (*it == '\0') /* Nu mai avem ce citi din buffer */
 				continue;
 			
-			ok_uid = 1; // am terminat de citit si uid-ul
-			if (kstrtol(str_uid, 10, &l_uid) != 0)
+			ok_uid = 1; /* Altfel, am dat de ':', deci am terminat de citit uid-ul */
+			if (kstrtol(str_uid, 10, &l_uid) != 0) /* Transformam string-ul in numar */
 			{
-				pr_err("Eroare parsare uid passwd!\n");
+				pr_err("UserFS: Eroare transformare string uid in int!\n");
 				goto err;
 			}
 			uid_passwd = KUIDT_INIT(l_uid);
@@ -114,7 +120,7 @@ static int get_username(kuid_t uid, char *name, int size)
 			goto done;
 		}
 	}
-	// daca am ajuns pana aici, inseamna ca nu am gasit nimic..	
+	/* Daca am ajuns pana aici, inseamna ca nu am gasit nimic..	*/
 	
 err:
 	filp_close(f, NULL);
@@ -125,7 +131,7 @@ done:
 	return 0;
 }
 
-/* Functie care creaza un nou inod si il initializeaza cu valori implicite */
+/* Creeaza un nou inod si initializeaza-l cu valori implicite */
 struct inode *userfs_new_inode(struct super_block *sb, umode_t mode)
 {
 	struct inode *inode = new_inode(sb); /* Functie a sistemului de operare care aloca un inod */
@@ -136,7 +142,6 @@ struct inode *userfs_new_inode(struct super_block *sb, umode_t mode)
 		inode->i_blocks = 0; /* Numarul de blocuri(nu avem niciun bloc cu date) */
 		inode->i_blkbits = inode->i_sb->s_blocksize_bits; /* Marimea unui block in biti(aceeasi ca a superblock-ului) */
 		inode->i_mtime = inode->i_atime = inode->i_ctime = current_time(inode); /* Timpul trunchiat la granularitatea fs-ului */
-// 		inode->i_uid = inode->i_gid = 0;
 		inode->i_ino = get_next_ino(); /* Numarul inodului */
 	}
 	
@@ -144,23 +149,23 @@ struct inode *userfs_new_inode(struct super_block *sb, umode_t mode)
 }
 
 
-/* Functia care se apeleaza la fiecare citire dintr-un fisier(procs) */ 
+/* Se apeleaza la fiecare citire din fisierul procs */ 
 static ssize_t userfs_file_read (struct file *file, char *usrbuf, 
 			size_t count, loff_t *offset)
 {
-	char *msg = kmalloc(MAX_OUTPUT_SIZE, GFP_KERNEL);
+	char *msg = kmalloc(USERFS_MAX_OUTPUT_SIZE, GFP_KERNEL);
 	char heading[] = "PID     Nume";
 	int bytes = 0;
 	struct task_struct *task;
 	struct dentry *parent = file_dentry(file)->d_parent;
 	char *username = parent->d_iname;
-	long dif = MAX_OUTPUT_SIZE - *offset;
-	ssize_t len = (count < dif) ? count : dif;
+	long dif = USERFS_MAX_OUTPUT_SIZE - *offset;
+	ssize_t len = (count < dif) ? count : dif; /* Cat citim */
 	
 	if (len <= 0)
-		return 0;
+		return 0; /* Marcheaza EOF */
 	
-	bytes += snprintf(msg, MAX_OUTPUT_SIZE, COLOR_GREEN "%s\n" COLOR_RESET, heading);
+	bytes += snprintf(msg, USERFS_MAX_OUTPUT_SIZE, COLOR_GREEN "%s\n" COLOR_RESET, heading);
 	
 	rcu_read_lock();
 	for_each_process(task)
@@ -175,60 +180,48 @@ static ssize_t userfs_file_read (struct file *file, char *usrbuf,
 		{
 			if (!strcmp(task_username, username))
 			{
-				if (bytes >= MAX_OUTPUT_SIZE)
+				if (bytes >= USERFS_MAX_OUTPUT_SIZE)
 				{
 					task_unlock(task);
 					break;
 				}
-				bytes += snprintf(msg + bytes, MAX_OUTPUT_SIZE - bytes, 
+				bytes += snprintf(msg + bytes, USERFS_MAX_OUTPUT_SIZE - bytes, 
 								"%-7d %s\n", (int) task_pid_nr(task), task->comm);
 			}
 		}
 		else
 		{
-			pr_err("Eroare aflare nume UID: %u\n", task_uid.val);
+			pr_err("UserFS: Eroare aflare nume UID: %u\n", task_uid.val);
+			return 0;
 		}
-		
 		task_unlock(task);
 		
 	}
 	rcu_read_unlock();
 		
-	if (*offset > bytes)
-		return 0;
+	if (*offset > bytes) /* Daca indicatorul de pozitie depaseste numarul de bytes pe care ii citim */
+		return 0; /* EOF */
 		
 	if (copy_to_user(usrbuf, msg + *offset, len) != 0)
 		return -1; /* mai degraba errno.. */
 	
 	kfree(msg);
 	
-	*offset += len;
+	*offset += len; /* Deplaseaza indicatorul de pozitie */
 	return len;
-}
-
-/* Functia care se apeleaza la fiecare deschidere a unui fisier(procs) */
-static int userfs_file_open (struct inode *inode, struct file *file)
-{
-	pr_info("USERFS: S-a deschis un fisier!\n");
-	
-	return 0;
 }
 
 void userfs_dentry_release(struct dentry *dentry)
 {
-	pr_info("UserFS: Se dezaloca dentry-ul: %s!\n", dentry->d_iname);
+	pr_info("UserFS: Se dezaloca dentry-ul: %s\n", dentry->d_iname);
 	if (dentry->d_inode)
-	{
-		pr_info("UserFS: Si inodul!\n");
 		clear_inode(dentry->d_inode);
-	}
 }
 
 
 /* Seteaza functiile ce vor fi apelate pentru diverse operatii I/O pe fisiere(procs) */
 static const struct file_operations userfs_file_ops = 
 {
-		.open = userfs_file_open,
 		.read = userfs_file_read
 };
 
@@ -238,7 +231,7 @@ static const struct dentry_operations userfs_dentry_ops =
 };
 
 
-/* Creeaza un fisier si asociaza-i o intrare de tip dentry.
+/* Creeaza un fisier procs si asociaza-i o intrare de tip dentry.
  * Foarte asemanatoare cu crearea unui director(userfs_create_dir).
  */
 int userfs_create_file(struct super_block *sb,
@@ -249,7 +242,7 @@ int userfs_create_file(struct super_block *sb,
 	struct qstr qname = QSTR_INIT(name, len);
 	umode_t mode = S_IRUGO;
 	
-	dentry = d_hash_and_lookup(parent, &qname); // se mareste ref count
+	dentry = d_hash_and_lookup(parent, &qname);
 	if (dentry && dentry->d_inode)
 	{
 		dput(dentry);
@@ -292,28 +285,26 @@ int userfs_create_dir(struct super_block *sb, struct dentry *parent,
 	struct qstr qname = QSTR_INIT(name, len);
 	umode_t mode = S_IFDIR | S_IXUGO | S_IRUGO; 
 	
-	dentry = d_hash_and_lookup(parent, &qname); // se mareste ref count
+	dentry = d_hash_and_lookup(parent, &qname); /* Cauta daca am creat deja intrarea */
 	if (dentry && dentry->d_inode)
 	{
 		int *fsdata = dentry->d_fsdata;
 		
-		if (*fsdata == tura) // am afisat deja directorul
+		if (*fsdata == turn) /* Am afisat deja directorul tura asta */
 		{
-			//pr_info("Am gasit deja tura asta: nr %ld, nume: %s\n", tura, name);
-			dput(dentry);
+			dput(dentry); /* Dupa hash_and_lookup trebuie scazut ref count-ul pt dentry */
 			return 0;
 		}
 		
-		//pr_info("Prima oara pe tura asta: nr %ld, nume: %s\n", tura, name);
-		*fsdata = tura; // marcheaza ca am afisat tura asta
+		*fsdata = turn; /* Marcheaza ca am afisat tura asta */
 		inode = dentry->d_inode;
 		dput(dentry);
 		ctx->pos += 1;
+		/* Paseaza informatii despre director catre cel care face citirea */
 		return dir_emit(ctx, name, len, inode->i_ino, inode->i_mode >> 12); 
 	}
 	if (!dentry) // daca nu intra pe ramura asta, inseamna ca dentryul nu are un inod asociat
 	{
-		//pr_info("Nu am gasit %s\n", name);
 		dentry = d_alloc(parent, &qname); /* Aloca o intrare dentry */
 		if (!dentry)
 			goto err;
@@ -324,34 +315,31 @@ int userfs_create_dir(struct super_block *sb, struct dentry *parent,
 	if (!inode)
 		goto err_free;
 	
-	ture[n] = tura; // am preluat lacatul din readdir
-	dentry->d_fsdata = &ture[n];
-	n = (n + 1) % MAX_USERS;
+	turns[n] = turn;
+	dentry->d_fsdata = &turns[n];
+	n = (n + 1) % USERFS_MAX_USERS;
 	
 	inode->i_fop = &simple_dir_operations; 
 	inode->i_op  = &simple_dir_inode_operations; 
-	d_add(dentry, inode); /* Adauga dentry-ul directorului creat in evidenta sistemului de operare */
+	d_add(dentry, inode); /* Asociasa dentry-ul cu inodul directorului */
 	
 	if (userfs_create_file(sb, dentry, "procs", 5) != 0)
 		goto err_free;
 		
 	dput(dentry);
 	ctx->pos += 1;
-	return dir_emit(ctx, name, len, inode->i_ino, inode->i_mode >> 12); // fill info
+	return dir_emit(ctx, name, len, inode->i_ino, inode->i_mode >> 12); /* Aici s-ar putea sa intoarca 1 la succes */
 	
 	err_free:
-		dput(dentry);//d_drop(dentry); /* Elibereaza intrarea daca am intampinat o eroare */
+		dput(dentry);
 	err:
 		return -1;
 }
 
 
-/* Creeaza directoarele pentru fiecare user si cate un fisier procs corespunzator */
+/* Se apeleaza la parcurgerea directorului radacina */
 static int userfs_root_readdir(struct file *file, struct dir_context *ctx)
 {
-	/* TODO: ATENTIE LA CONDITIILE DE CURSA! */
-	/* TODO: MOMENTAN MERGE, DAR E CAM INEFICIENT(SE ALOCA MEMORIE IN PROSTIE!), DE MEMORY LEAK-URI NICI NU MAI VORBIM..*/
-	
 	struct super_block *sb = file_inode(file)->i_sb;
 	struct dentry *root = sb->s_root;
 	struct task_struct *task;
@@ -362,43 +350,42 @@ static int userfs_root_readdir(struct file *file, struct dir_context *ctx)
 	if (!dir_emit_dots(file, ctx)) /* trecem peste '.' si '..' */
 		return 0;
 	
-	mutex_lock(&userfs_mutex_tura);
+	mutex_lock(&userfs_mutex_turn);
 	rcu_read_lock();
 	for_each_process(task)
 	{
-		//char name[20]; /* TODO:  de schimbat aici */
 		char username[33];
 		int len;
 		kuid_t uid;
 		
 		task_lock(task);
-		uid = task->cred->uid; /* Preia id-ul utilizatorului */
+		uid = task->cred->uid; /* Preia uid-ul procesului */
 		task_unlock(task);
 		
 		get_username(uid, username, sizeof(username));
-		//len = snprintf(name, sizeof(name), "%u", uid.val);
 		if (!get_username(uid, username, sizeof(username)))
 		{
 			len = strlen(username);
-			userfs_create_dir(sb, root, ctx, username, len); // de verificat si aici eroare
+			userfs_create_dir(sb, root, ctx, username, len); /* Aici de verificat eroare */
 		}
 		else
 		{
-			pr_err("Eroare aflare nume UID: %u\n", uid.val);
+			pr_err("UserFS: Eroare aflare nume UID: %u\n", uid.val);
 		}
 	}
 	rcu_read_unlock();
-	tura++;
-	mutex_unlock(&userfs_mutex_tura);
+	turn++;
+	mutex_unlock(&userfs_mutex_turn);
 	
-	ctx->pos = USERFS_DIRS_OFFSET + 1;
+	ctx->pos = USERFS_DIRS_OFFSET + 1; /* Marcam ca am terminat parcurgerea */
 	return 0;
 }
 
+/* Se apeleaza cand se cauta o intrare specifica din directorul radacina */
 static struct dentry *userfs_root_lookup(struct inode *dir, struct dentry *dentry, unsigned int flags)
 {
 	struct task_struct *task;
-	const char *username = dentry->d_iname;
+	const char *username = dentry->d_iname; /* Numele cautat */
 	
 	rcu_read_lock();
 	for_each_process(task)
@@ -407,7 +394,7 @@ static struct dentry *userfs_root_lookup(struct inode *dir, struct dentry *dentr
 		kuid_t task_uid;
 		
 		task_lock(task);
-		task_uid = task->cred->uid; /* Preia id-ul utilizatorului */
+		task_uid = task->cred->uid; /* Preia uid-ul procesului*/
 		task_unlock(task);
 		
 		if (!get_username(task_uid, task_username, sizeof(username)))
@@ -419,7 +406,7 @@ static struct dentry *userfs_root_lookup(struct inode *dir, struct dentry *dentr
 				
 				if (!inode)
 				{
-					pr_err("Eroare alocare inod lookup\n");
+					pr_err("UserFS: Eroare alocare inod lookup\n");
 					return ERR_PTR(-1);
 				}
 				
@@ -427,10 +414,10 @@ static struct dentry *userfs_root_lookup(struct inode *dir, struct dentry *dentr
 				inode->i_fop = &simple_dir_operations;
 				inode->i_op = &simple_dir_inode_operations;
 				
-				mutex_lock(&userfs_mutex_tura);
-				dentry->d_fsdata = &ture[n];
-				n = (n + 1) % MAX_USERS;
-				mutex_unlock(&userfs_mutex_tura);
+				mutex_lock(&userfs_mutex_turn);
+				dentry->d_fsdata = &turns[n];
+				n = (n + 1) % USERFS_MAX_USERS;
+				mutex_unlock(&userfs_mutex_turn);
 				
 				d_add(dentry, inode);
 				userfs_create_file(dir->i_sb, dentry, "procs", 5);
@@ -442,7 +429,7 @@ static struct dentry *userfs_root_lookup(struct inode *dir, struct dentry *dentr
 		}
 		else
 		{
-			pr_err("Eroare aflare nume UID: %u\n", task_uid.val);
+			pr_err("UserFS: Eroare aflare nume UID: %u\n", task_uid.val);
 		}
 		
 	}
@@ -457,8 +444,6 @@ const struct super_operations userfs_sb_ops =
 {
 	.statfs    = simple_statfs,         /* Output pentru comenzile 'stat -f' sau 'df' */
 	.drop_inode = generic_delete_inode  /* Se apeleaza la stergerea unui inod */
-	
-	/* TODO: show_options */
 };
 
 static const struct file_operations userfs_root_ops =
@@ -474,7 +459,7 @@ static const struct inode_operations userfs_root_inode_ops =
 };
 
 
-/* Functie care initializeaza superblock-ul sistemului de fisiere */
+/* Initializeaza superblock-ul sistemului de fisiere(practic metadate despre acesta) */
 int userfs_fill_sb(struct super_block *sb, void *data, int silent)
 {
 	struct inode *inode;
@@ -492,9 +477,8 @@ int userfs_fill_sb(struct super_block *sb, void *data, int silent)
 	
 	if (!sb->s_root)
 	{
-		pr_err("Eroare alocare inod!\n");
-// 		return -ENOMEM; /* Out of memory */
-		return -1;
+		pr_err("UserFS: Eroare alocare inod!\n");
+		return -ENOMEM;
 	}
 
 	return 0;
@@ -502,7 +486,7 @@ int userfs_fill_sb(struct super_block *sb, void *data, int silent)
 
 
 
-/* Functie care intoarce un pointer catre directorul radacina */
+/* Intoarce un pointer catre directorul radacina */
 struct dentry *userfs_mount(struct file_system_type *fs_type,
 		int flags, const char *dev_name, void *data)
 {
@@ -528,15 +512,15 @@ static struct file_system_type userfs_type =
 static int userfs_init(void)
 {
 	/* Inregistreaza sistemul de fisiere in evidenta sistemului de operare */
+	int errno;
 	
-	if (register_filesystem(&userfs_type) != 0)
+	if ((errno = register_filesystem(&userfs_type)) != 0)
 	{
-		pr_err("Eroare la inregistrarea UserFS!\n");
-// 		return errno;
-		return -1;
+		pr_err("UserFS: Eroare la inregistrarea UserFS!\n");
+		return errno;
 	}
 	
-	pr_info("Inregistrare UserFS - Succes!\n");
+	pr_info("UserFS: Inregistrare - Succes!\n");
 	return 0;
 }
 
@@ -545,9 +529,9 @@ static void userfs_exit(void)
 	/* Scoate sistemul de fisiere din evidenta sistemului de operare */
 	
 	if (unregister_filesystem(&userfs_type) != 0)
-		pr_err("Eroare la deinregistrare\n"); //TODO: De schimbat mesajul!
+		pr_err("UserFS: Eroare la deinregistrare\n");
 	else
-		pr_info("Deinregistrare UserFS - Succes!\n");
+		pr_info("UserFS: Deinregistrare - Succes!\n");
 	
 }
 
